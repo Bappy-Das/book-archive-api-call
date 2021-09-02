00cbc49431492c62e6c2382114787cae4b96cebc
Bappy-Das <bappydas.bd51@gmail.com>
2021-09-02 12:34:55 +0600

    error handle

--- js/app.js
+++ js/app.js
@@ -18,10 +18,31 @@ const searchClick = () => {
 
 const displayBook = books => {
     document.getElementById('total-search').innerText = books.numFound
+    display.textContent = '';
+
+    if (books.docs.length === 0) {
+        display.innerHTML = `
+        <div class="col-md-12 text-center">
+            <div class="error-template">
+                <h1>
+                    Oops!</h1>
+                <h2>
+                    404 Not Found</h2>
+                <div class="error-details">
+                    Sorry, The page you requested was not found!
+                </div>
+            </div>
+        </div>
+        `;
+    }
+
+    // get single book
     books.docs.forEach(book => {
         // console.log(book);
-        // console.log(book.author_name)
         const cover = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
+
+
+        // dynamic card
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
@@ -30,11 +51,12 @@ const displayBook = books => {
                 <div class="col-md-4 p-2">
                     <img src=${cover} class="card-img-top" style="height: 200px;" alt="...">
                 </div>
-                <div class="col-md-8">
+                <div class="col-md-6">
                     <div class="card-body">
-                        <h5 class="">Name : ${book.title}</h5>
-                        <p class="card-text">Author : ${book.author_name}</p>
+                        <h5 class="">${book.title}</h5>
+                        <p class="card-text">By ${book.author_name}</p>
                         <p class="card-text"><small class="text-muted">First published in ${book.first_publish_year}</small></p>
+                        <p class="card-text"><small class="text-muted">Publisher : ${book.publisher}</small></p>
                     </div>
                 </div>
             </div>
