5694b1ded5a0223789ef97731aa2ed7459c37ad6
Bappy-Das <bappydas.bd51@gmail.com>
2021-09-02 19:29:33 +0600

    total result add

--- js/app.js
+++ js/app.js
@@ -1,29 +1,40 @@
 const inputText = document.getElementById('input-text');
 const display = document.getElementById('books');
-const bookImage = document.getElementById('book-img');
-const bookName = document.getElementById('book-name');
-const authorName = document.getElementById('author-name');
-const publication = document.getElementById('first-piblication');
 
 const searchClick = () => {
     const text = inputText.value;
     inputText.value = '';
-    // console.log(text);
     const url = `https://openlibrary.org/search.json?q=${text}`;
-    // console.log(url)
     fetch(url)
         .then(res => res.json())
         .then(data => displayBook(data))
 }
 
 const displayBook = books => {
-    document.getElementById('total-search').innerText = books.numFound
-    display.textContent = '';
 
+    // total search result
+    const total = document.getElementById('total-result');
+    total.textContent = '';
+    const p = document.createElement('p');
+    p.innerHTML = `
+        <div class="card w-75 mx-auto">
+            <div class="card-body text-center text-uppercase fw-bold">
+                Search Found ${books.numFound} Books.
+            </div>
+        </div>
+    
+    
+    
+    `;
+    total.appendChild(p);
+
+    // empty full content
+    display.textContent = '';
+    // error handling
     if (books.docs.length === 0) {
         display.innerHTML = `
-        <div class="col-md-12 text-center">
-            <div class="error-template">
+        <div class="col-md-12 text-center mt-5">
+            <div class="error-template mt-5">
                 <h1>
                     Oops!</h1>
                 <h2>
@@ -38,9 +49,11 @@ const displayBook = books => {
 
     // get single book
     books.docs.forEach(book => {
-        // console.log(book);
-        const cover = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
 
+        console.log(books)
+        console.log(books.length)
+        // book image
+        const cover = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
 
         // dynamic card
         const div = document.createElement('div');
@@ -56,7 +69,7 @@ const displayBook = books => {
                         <h5 class="">${book.title}</h5>
                         <p class="card-text">By ${book.author_name}</p>
                         <p class="card-text"><small class="text-muted">First published in ${book.first_publish_year}</small></p>
-                        <p class="card-text"><small class="text-muted">Publisher : ${book.publisher}</small></p>
+                        <p class="card-text"><small class="text-muted">Publisher : ${book.publisher[0]}</small></p>
                     </div>
                 </div>
             </div>
